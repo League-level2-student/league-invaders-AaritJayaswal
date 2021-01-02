d6c19112b7ba7f7ffe9d1e709b31f27c75e1f8e8
AaritJayaswal <55632549+AaritJayaswal@users.noreply.github.com>
2021-01-02 13:49:28 -0800

    Update LeagueInvaders.java

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,26 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	
+	JFrame frame = new JFrame();
 
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 800;
+	
+	LeagueInvaders(){
+		this.frame = frame;
+	}
+	
+	public static void main(String[]args){
+		
+		
+		
+	}
+	
+	void setup() {
+		
+	frame.setVisible(true);
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	}
 }
