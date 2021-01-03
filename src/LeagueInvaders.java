e5170c3f265a851e7657bc6db6aaa996c0243887
AaritJayaswal <55632549+AaritJayaswal@users.noreply.github.com>
2021-01-03 12:55:58 -0800

    Update LeagueInvaders.java

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,16 +1,29 @@
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 
 public class LeagueInvaders {
 	
-	JFrame frame = new JFrame();
-
+	
 	public static final int WIDTH = 500;
 	public static final int HEIGHT = 800;
 	
-	LeagueInvaders(){
-		this.frame = frame;
-	}
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+
 	
+	 public LeagueInvaders(){
+		
+		 LeagueInvaders invaders = new LeagueInvaders();
+		 invaders.setup();
+
+	
+	 }
+	 
+	 
+	 
 	public static void main(String[]args){
 		
 		
@@ -20,7 +33,9 @@ public class LeagueInvaders {
 	void setup() {
 		
 	frame.setVisible(true);
-frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.add(panel);
+	 frame.getContentPane().setPreferredSize(new Dimension(WIDTH,HEIGHT));
+	frame.pack();
 	}
 }
