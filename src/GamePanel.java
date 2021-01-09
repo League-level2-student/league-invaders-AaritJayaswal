a7a47d05a86637bf48a843eb2b9830e2afaeb4d9
AaritJayaswal <55632549+AaritJayaswal@users.noreply.github.com>
2021-01-09 12:58:37 -0800

    worked on league invaders

---
+++ src/GamePanel.java
@@ -0,0 +1,80 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
+import javax.swing.*;
+public class GamePanel extends JPanel {
+	public Font titleFont;
+	
+	public Font textFont;
+	
+	final int MENU = 0;
+    final int GAME = 1;
+    final int END = 2;
+	
+    int currentState = MENU;
+
+    public GamePanel() {
+    	
+    	titleFont = new Font("Arial", Font.PLAIN, 48);
+    	
+    	textFont = new Font("Arial", Font.PLAIN, 20);
+    }
+    
+    
+    void updateMenuState() { 
+    	
+    	
+    }
+   
+    void updateGameState() {  
+    	
+    	
+    }
+    
+    void updateEndState()  {  
+    	
+    	
+    }
+    
+    void drawMenuState(Graphics g) {
+    	g.setColor(Color.BLUE);
+    	g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+    	
+    	g.setFont(titleFont);
+    	g.setColor(Color.YELLOW);
+    	g.drawString("LEAGUE INVADERS", 20, 50);
+    
+    	g.setFont(textFont);
+    	g.setColor(Color.YELLOW);
+    	g.drawString("Press ENTER to start", 160, 140);
+    	g.drawString("Press SPACE for instructions", 120, 300);
+    }
+    
+    void drawGameState(Graphics g) {
+    	
+    	
+    }
+    
+    void drawEndState(Graphics g)  {
+    	
+    	
+    }
+    
+    @Override
+	public void paintComponent(Graphics g){
+
+		
+    	if(currentState == MENU){
+    	    drawMenuState(g);
+    	}
+    	else if(currentState == GAME){
+    	    drawGameState(g);
+    	}
+    	else if(currentState == END){
+    	    drawEndState(g);
+    	}
+		
+	}
+	
+}
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,24 +1,19 @@
 import java.awt.Dimension;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
+import javax.swing.*;
 
 public class LeagueInvaders {
 	
-	
+	public JFrame frame;
+	public GamePanel panel;
 	public static final int WIDTH = 500;
 	public static final int HEIGHT = 800;
 	
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
+	
 
 	
 	 public LeagueInvaders(){
-		
-		 LeagueInvaders invaders = new LeagueInvaders();
-		 invaders.setup();
-
+		frame = new JFrame();
+		 panel = new GamePanel();
 	
 	 }
 	 
@@ -26,15 +21,17 @@ public class LeagueInvaders {
 	 
 	public static void main(String[]args){
 		
+		LeagueInvaders invaders = new LeagueInvaders();
+		 invaders.setup();
 		
 		
 	}
 	
 	void setup() {
-		
-	frame.setVisible(true);
+frame.add(panel);
+		frame.setVisible(true);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.add(panel);
+
 	 frame.getContentPane().setPreferredSize(new Dimension(WIDTH,HEIGHT));
 	frame.pack();
 	}
