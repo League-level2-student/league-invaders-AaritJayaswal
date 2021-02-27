d5161bea42dc4b97801274e7a8d1b75c3375416b
AaritJayaswal <55632549+AaritJayaswal@users.noreply.github.com>
2021-02-27 12:59:06 -0800

    Update ObjectManager.java

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ObjectManager {
 
@@ -8,4 +10,36 @@ public class ObjectManager {
 		ship=ship1;
 	}
 	
+	
+	public ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
+
+	public ArrayList<Alien> aliens = new ArrayList<Alien>();
+
+	
+	
+	Random random = new Random();
+	
+void addProjectile(Projectile p) {
+	
+	projectiles.add(p);
+
+}
+
+void addAlien(Alien a) {
+	
+	aliens.add(new Alien(random.nextInt(LeagueInvaders.WIDTH),0,50,50));
+	
+}
+
+void update(){
+	
+	for(Alien a: aliens) {
+		
+		update();
+	}
+	
+}
+
+int AlienY = alien.y1();
+
 }
