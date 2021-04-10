1becd89c0e4ca4b91714eff143437ed2d8b4e68b
AaritJayaswal <55632549+AaritJayaswal@users.noreply.github.com>
2021-04-10 12:57:26 -0700

    worked on league invaders

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -7,207 +7,186 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.*;
-public class GamePanel extends JPanel implements ActionListener, KeyListener{
-	
 
-	
-   	public Rocketship ship = new Rocketship(250, 750, 50, 50);
-   
-    public Timer frameDraw;
-   
-   	public Font titleFont;
-   
-    public Font textFont;
-   
-   	final int MENU = 0;
-    final int GAME = 1;
-    final int END = 2;
-   	
-    int currentState = MENU;
+public class GamePanel extends JPanel implements ActionListener, KeyListener {
 
-    public GamePanel() {
-    	frameDraw = new Timer(1000/60,this);
-        frameDraw.start();
-        
-    	titleFont = new Font("Arial", Font.PLAIN, 48);
-    	
-    	textFont = new Font("Arial", Font.PLAIN, 20);
-   
-  
-    }
-    
-    public ObjectManager objectManager = new ObjectManager(ship);
-    
-    void updateMenuState() { 
-    	
-    	
-    }
-   
-    void updateGameState() {  
-    	
-    	objectManager.update();
-    	
-    }
-    
-    void updateEndState()  {  
-   
-    	
-    }
-    
-    void drawMenuState(Graphics g) {
-    	g.setColor(Color.BLUE);
-    	g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
-    	
-    	g.setFont(titleFont);
-    	g.setColor(Color.YELLOW);
-    	g.drawString("LEAGUE INVADERS", 20, 50);
-    
-    	g.setFont(textFont);
-    	g.setColor(Color.YELLOW);
-    	g.drawString("Press ENTER to start", 160, 140);
-    	g.drawString("Press SPACE for instructions", 120, 300);
-    }
+	public Rocketship ship = new Rocketship(250, 750, 50, 50);
 
-    void drawGameState(Graphics g) {
-    	g.setColor(Color.BLACK);
-    	g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
-    	objectManager.draw(g);
-    	
-    }
-    
-    void drawEndState(Graphics g)  {
-    	g.setColor(Color.RED);
-    	g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
-    	
-    	g.setFont(titleFont);
-    	g.setColor(Color.BLACK);
-    	g.drawString("GAME OVER", 100, 50);
-    
-    	g.setFont(textFont);
-    	g.setColor(Color.BLACK);
-    	g.drawString("You killed number enemies", 130, 140);
-    	g.drawString("Press ENTER to restart", 140, 300);
-    	
-    }
-    
-    Timer alienSpawn;
-    
-    void startGame() {
-    alienSpawn = new Timer(1000,objectManager);
-    alienSpawn.start();
-    	
-    }
-    
-    
-    @Override
-	public void paintComponent(Graphics g){
+	public Timer frameDraw;
 
-		
-    	if(currentState == MENU){
-    	    drawMenuState(g);
-    	}
-    	else if(currentState == GAME){
-    	    drawGameState(g);
-    	}
-    	else if(currentState == END){
-    	    drawEndState(g);
-    	}
-		
+	public Font titleFont;
+
+	public Font textFont;
+
+	final int MENU = 0;
+	final int GAME = 1;
+	final int END = 2;
+
+	int currentState = MENU;
+
+	public GamePanel() {
+		frameDraw = new Timer(1000 / 60, this);
+		frameDraw.start();
+
+		titleFont = new Font("Arial", Font.PLAIN, 48);
+
+		textFont = new Font("Arial", Font.PLAIN, 20);
+
 	}
 
+	public ObjectManager objectManager = new ObjectManager(ship);
+
+	void updateMenuState() {
+
+	}
+
+	void updateGameState() {
+
+		objectManager.update();
+
+	}
+
+	void updateEndState() {
+
+	}
+
+	void drawMenuState(Graphics g) {
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+
+		g.setFont(titleFont);
+		g.setColor(Color.YELLOW);
+		g.drawString("LEAGUE INVADERS", 20, 50);
+
+		g.setFont(textFont);
+		g.setColor(Color.YELLOW);
+		g.drawString("Press ENTER to start", 160, 140);
+		g.drawString("Press SPACE for instructions", 120, 300);
+	}
+
+	void drawGameState(Graphics g) {
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		objectManager.draw(g);
+
+	}
+
+	void drawEndState(Graphics g) {
+		g.setColor(Color.RED);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+
+		g.setFont(titleFont);
+		g.setColor(Color.BLACK);
+		g.drawString("GAME OVER", 100, 50);
+
+		g.setFont(textFont);
+		g.setColor(Color.BLACK);
+		g.drawString("You killed number enemies", 130, 140);
+		g.drawString("Press ENTER to restart", 140, 300);
+
+	}
+
+	Timer alienSpawn;
+
+	void startGame() {
+		alienSpawn = new Timer(1000, objectManager);
+		alienSpawn.start();
+
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+
+		if (currentState == MENU) {
+			drawMenuState(g);
+		} else if (currentState == GAME) {
+			drawGameState(g);
+		} else if (currentState == END) {
+			drawEndState(g);
+		}
+
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-		
-		if(currentState == MENU){
-		    updateMenuState();
-		}else if(currentState == GAME){
-			
+
+		if (currentState == MENU) {
+			updateMenuState();
+		} else if (currentState == GAME) {
+
 			updateGameState();
-		}else if(currentState == END){
-		  
+		} else if (currentState == END) {
+
 			updateEndState();
 		}
-	
+
 		repaint();
 	}
-	
-	
-	
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		if (e.getKeyCode()==KeyEvent.VK_ENTER) {
-		    if (currentState == END) {
-		        currentState = MENU;
-		    	}
-		     
-		    
-		    
-		    else{
+		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+			if (currentState == END) {
+				currentState = MENU;
+			}
+
+			else {
 				currentState++;
-				objectManager.ship = new Rocketship(250,700,50,50);
-				
-			 if (currentState == GAME) {
-			    	startGame();
-			    	
-			    }
-			
-			 else if (currentState == END) {
-				  alienSpawn.stop();
-				 
-			 }
-			 
-		    }
+				objectManager.ship = new Rocketship(250, 700, 50, 50);
+
+				if (currentState == GAME) {
+					startGame();
+
+				}
+
+				else if (currentState == END) {
+					alienSpawn.stop();
+
+				}
+
+			}
 		}
-		
-		if(currentState == GAME) {
-		
-		 if (e.getKeyCode()==KeyEvent.VK_SPACE) {
-			 	objectManager.addProjectile(ship.getProjectile());
-			 }
-			 
-		 
-		    
-		
-		if (e.getKeyCode()==KeyEvent.VK_UP) {
-	
-		 objectManager.ship.up();
-		   
+
+		if (currentState == GAME) {
+
+			if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+				objectManager.addProjectile(objectManager.ship.getProjectile());
+			}
+
+			if (e.getKeyCode() == KeyEvent.VK_UP) {
+
+				objectManager.ship.up();
+
+			}
+			if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+
+				objectManager.ship.down();
+
+			}
+			if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+
+				objectManager.ship.right();
+			}
+			if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+
+				objectManager.ship.left();
+
+			}
 		}
-		if (e.getKeyCode()==KeyEvent.VK_DOWN) {
-		
-			 objectManager.ship.down();
-		    
-		   
-		    
-		}
-		if (e.getKeyCode()==KeyEvent.VK_RIGHT) {
-		   
-		  
-		   objectManager.ship.right();
-		}
-		 if (e.getKeyCode()==KeyEvent.VK_LEFT) {
-		        
-		      
-		        objectManager.ship.left();
-		        
-		    }    
-	}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
-	}
 
+	}
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -4,114 +4,108 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class ObjectManager implements ActionListener{
+public class ObjectManager implements ActionListener {
 
 	public Rocketship ship;
-	
+
 	public ObjectManager(Rocketship ship1) {
-		
-		ship=ship1;
+
+		ship = ship1;
 	}
-	
-	
+
 	public ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
 
 	public ArrayList<Alien> aliens = new ArrayList<Alien>();
 
-	
-	
 	Random random = new Random();
-	
-void addProjectile(Projectile p) {
-	
-	projectiles.add(p);
 
-}
+	void addProjectile(Projectile p) {
 
-void addAlien() {
-	
-	aliens.add(new Alien(random.nextInt(LeagueInvaders.WIDTH),0,50,50));
-	
-}
+		projectiles.add(p);
 
-void update(){
-	
-	for(Alien a: aliens) {
-		
-		a.update();
-		int alienY = a.y;
-		
-		if(LeagueInvaders.HEIGHT<alienY) {
-			
-			
-			a.isActive = false;
-			
+	}
+
+	void addAlien() {
+
+		aliens.add(new Alien(random.nextInt(LeagueInvaders.WIDTH), 0, 50, 50));
+
+	}
+
+	void update() {
+
+		for (Alien a : aliens) {
+
+			a.update();
+			int alienY = a.y;
+
+			if (LeagueInvaders.HEIGHT < alienY) {
+
+				a.isActive = false;
+
+			}
+
 		}
-		
-		for(Projectile p: projectiles) {
-			
+
+		for (Projectile p : projectiles) {
+
 			p.update();
 			int projectileY = p.y;
-			if(LeagueInvaders.HEIGHT>projectileY) {
-				
+			if (LeagueInvaders.HEIGHT < projectileY) {
+
 				p.isActive = false;
-				
-				
+
 			}
 		}
-		
-		
+
 	}
-	
-}
 
-void draw(Graphics g){
-	
-	ship.draw(g);
-	
-	for(Alien a: aliens) {
-		
-		a.draw(g);
-	}
-	
-}
+	void draw(Graphics g) {
 
+		ship.draw(g);
 
-void purgeObjects() {
-	
-	for(Alien a: aliens) {
-		
-		if(a.isActive == false) {
-	
-		aliens.remove(a);
+		for (Alien a : aliens) {
+
+			a.draw(g);
+		}
+
+		for (Projectile p : projectiles) {
+			
+			p.draw(g);
 			
 		}
-	
-	
-}
-
-	for(Projectile p: projectiles) {
 		
-		if(p.isActive == false) {
-			
-			projectiles.remove(p);
-		
-		
-		}
 		
 	}
-	
-}
 
+	void purgeObjects() {
 
+		for (Alien a : aliens) {
 
-@Override
-public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
-	addAlien();
-	
-}
+			if (a.isActive == false) {
 
+				aliens.remove(a);
 
+			}
+
+		}
+
+		for (Projectile p : projectiles) {
+
+			if (p.isActive == false) {
+
+				projectiles.remove(p);
+
+			}
+
+		}
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		addAlien();
+
+	}
 
 }
